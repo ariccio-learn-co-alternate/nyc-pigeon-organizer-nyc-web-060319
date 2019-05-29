dc7fba5242bb400b39b4cce31742e1dadf88bc51
Alexander Riccio <test35965@gmail.com>
2019-05-29 22:53:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.color.purple.each do |pigeon_name|
-    if pigeon_list.has
+    if pigeon_list.has_key?(pigeon_name)
+      
   end
 end
