70147fda28afdd409d0d4f25d0c2606b91d9ceac
Alexander Riccio <test35965@gmail.com>
2019-05-29 23:06:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list_out = {}
-  data.color.each do |color, pigeons_list|
+  data[:color].each do |color, pigeons_list|
     pigeons_list.each do |pigeon_name|
       if pigeon_list_out.has_key?(pigeon_name)
         pigeon_list_out[pigeon_name][:color] << color
@@ -10,12 +10,12 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  data.gender.each do |gender, pigeons_list|
+  data[:gender].each do |gender, pigeons_list|
     pigeons_list.each do |pigeon_name|
       pigeon_list_out[pigeon_name][:gender] = gender
     end
   end
-  data.lives.each do |location, pigeons_list|
+  data[:lives].each do |location, pigeons_list|
     pigeons_list.each do |pigeon_name|
       if pigeon_list_out[pigeon_name].has_key?(:lives)
         pigeon_list_out[pigeon_name][:lives] << location
