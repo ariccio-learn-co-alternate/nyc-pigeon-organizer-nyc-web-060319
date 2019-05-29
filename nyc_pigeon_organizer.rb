58586b9f8e2e09d36e65ec5c51fb82a28e746859
Alexander Riccio <test35965@gmail.com>
2019-05-29 22:59:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.color.each do |color, pigeons_list|
     pigeons_list.each do |pigeon_name|
       if pigeon_list.has_key?(pigeon_name)
-        pigeon_list[pigeon_name][:color] = color
+        pigeon_list[pigeon_name][:color] << color
       else
         pigeon_list[pigeon_name] = {
           :color => [color]
