be1c352633192fb023625415ed0b08429656b4fd
Alexander Riccio <test35965@gmail.com>
2019-05-29 23:03:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,11 +12,10 @@ def nyc_pigeon_organizer(data)
   end
   data.gender.each do |gender, pigeons_list|
     pigeons_list.each do |pigeon_name|
-      if pigeon_list_out.has_key?(pigeon_name)
-        pigeon_list_out[pigeon_name][:gender] = gender
-      else
-        pigeon_list_out[pigeon_name]
-      end
+      pigeon_list_out[pigeon_name][:gender] = gender
     end
+  end
+  data.lives.each do |location, pigeons_list|
+    pigeons_list.each do |pigeon_name|
   end
 end
